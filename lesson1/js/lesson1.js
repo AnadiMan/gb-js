5afb47931583db6728bb12d12d744eabec66337a
Evgeniy Agafonov <e.agafonov@itgene.ru>
2020-10-15 14:43:43 +0300

    lesson 1 is done

---
+++ lesson1/js/lesson1.js
@@ -0,0 +1,22 @@
+// let a = prompt('Enter you name')
+
+let tC = prompt('Введите значение температуры по Цельсию')
+let tF = (9 / 5) * tC + 32
+
+document.write('<p>' + 'Температура по Фарингейту = ' + tF + '</p>');
+
+let admin
+let name
+name = 'Василий'
+admin = name
+
+document.write('<p>' + 'Admin ' + admin + ', добро пожаловать!' + '<p>')
+
+let question = 'Чему будет равно JS-выражение 1000 + "108"?'
+let a = 1000 
+let b = "108"
+let answer = a + b
+
+document.write('<p>' + question + '<p>')
+document.write('<p>' + 'Ответ = ' + answer + '<p>')
+
